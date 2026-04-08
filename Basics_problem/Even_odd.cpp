3af8e249a61bfc363c0c77ea4bb5be839193ea3f
tashiraj4-stack <tashiraj4@gmail.com>
2026-04-08 14:47:30 +0530

    Create Even_odd.cpp

---
+++ Basics_problem/Even_odd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a;
+    
+    cout<<"enter the value of a:";
+    cin>>a;
+
+    if(a %2 == 0) cout<<"The number is Even" <<endl;
+    else cout<<"The number is Odd" <<endl;
+        return 0;
+    }
+
+
